6c947e15f5f2347a0032b0f0c96471595b8d93f6
Robert Israelsson <robertisraelsson@Roberts-iMac.local>
2018-12-28 23:47:46 +0100

    Added Firebase functionallity to the app

--- android/app/src/main/java/com/bidragskollen/bidragskollen/MainApplication.java
+++ android/app/src/main/java/com/bidragskollen/bidragskollen/MainApplication.java
@@ -1,4 +1,4 @@
-package com.invertase.rnfirebasestarter;
+package com.bidragskollen.bidragskollen;
 
 import android.annotation.SuppressLint;
 import android.app.Application;
--- ios/BidragskollenTests/BidragskollenTests.m
+++ ios/BidragskollenTests/BidragskollenTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface RNFirebaseStarterTests : XCTestCase
+@interface BidragskollenTests : XCTestCase
 
 @end
 
-@implementation RNFirebaseStarterTests
+@implementation BidragskollenTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
